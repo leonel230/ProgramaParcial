9f706b7abc566750b6062da6ef35d97ab0010bd4
TagoSMD <jacobgt890@gmail.com>
2020-04-11 22:23:43 -0600

    Cuarto Boton

--- NAYRE/Programa Parcial 1/src/lab2.java
+++ NAYRE/Programa Parcial 1/src/lab2.java
@@ -74,6 +74,7 @@ PreparedStatement pst=null;
         jMenu6 = new javax.swing.JMenu();
         jMenuItem10 = new javax.swing.JMenuItem();
         jMenuItem2 = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(204, 204, 204));
@@ -405,6 +406,14 @@ PreparedStatement pst=null;
             }
         });
         jMenu6.add(jMenuItem2);
+
+        jMenuItem3.setText("Canal de youtube4");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
+        jMenu6.add(jMenuItem3);
 
         jMenuBar1.add(jMenu6);
 
@@ -756,6 +765,22 @@ PreparedStatement pst=null;
   }
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+     
+                   Desktop browser = Desktop.getDesktop();
+  try{
+    try {
+        browser.browse(new URI("https://www.youtube.com/watch?v=3va32312uM7axnRs1111"));
+    } catch (URISyntaxException ex) {
+       
+    }
+  }
+  catch(IOException err){
+      
+  }
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
+
     
     public static void main(String args[]) {
       
@@ -791,6 +816,7 @@ PreparedStatement pst=null;
     private javax.swing.JMenuItem jMenuItem10;
     private javax.swing.JMenuItem jMenuItem19;
     private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
     private javax.swing.JMenuItem jMenuItem5;
     private javax.swing.JMenuItem jMenuItem8;
